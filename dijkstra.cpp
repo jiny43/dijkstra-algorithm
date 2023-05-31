b9ade3668155d1a94d8883221489dd2718ec0239
jieun <jiny273588@gmail.com>
2023-05-31 10:47:33 +0900

    B: 5 A: 0 C: 3 E: 15 D: 7

--- dijkstra.cpp
+++ dijkstra.cpp
@@ -1,39 +1,62 @@
-import heapq
+#include <iostream>
+#include <queue>
+#include <unordered_map>
+#include <limits>
 
-def dijkstra(graph, start):
-    # 시작 노드에서부터의 최단 거리를 저장하는 딕셔너리
-    distances = {node: float('inf') for node in graph}
-    distances[start] = 0  # 시작 노드의 최단 거리는 0
-    queue = [(0, start)]  # 우선순위 큐를 사용하여 처리할 노드들을 저장
+using namespace std;
 
-    while queue:
-        current_distance, current_node = heapq.heappop(queue)  # 최단 거리가 가장 작은 노드 선택
-
-        # 이미 처리된 노드라면 건너뜀
-        if current_distance > distances[current_node]:
-            continue
-
-        # 현재 노드와 연결된 인접한 노드들을 순회
-        for adjacent_node, weight in graph[current_node].items():
-            distance = current_distance + weight  # 현재 노드를 거쳐 가는 경로의 거리
-
-            # 더 짧은 경로를 찾았을 때 업데이트
-            if distance < distances[adjacent_node]:
-                distances[adjacent_node] = distance
-                heapq.heappush(queue, (distance, adjacent_node))
-
-    return distances
-
-# 그래프 생성 (인접 리스트로 표현)
-graph = {
-    'A': {'B': 5, 'C': 3},
-    'B': {'A': 5, 'D': 2},
-    'C': {'A': 3, 'D': 6},
-    'D': {'B': 2, 'C': 6, 'E': 8},
-    'E': {'D': 8}
+unordered_map<char, unordered_map<char, int>> createGraph() {
+    unordered_map<char, unordered_map<char, int>> graph;
+    graph['A'] = {{'B', 5}, {'C', 3}};
+    graph['B'] = {{'A', 5}, {'D', 2}};
+    graph['C'] = {{'A', 3}, {'D', 6}};
+    graph['D'] = {{'B', 2}, {'C', 6}, {'E', 8}};
+    graph['E'] = {{'D', 8}};
+    return graph;
 }
 
-start_node = 'A'
-distances = dijkstra(graph, start_node)
+unordered_map<char, int> dijkstra(unordered_map<char, unordered_map<char, int>>& graph, char start) {
+    unordered_map<char, int> distances;
+    for (const auto& pair : graph) {
+        distances[pair.first] = numeric_limits<int>::max();
+    }
+    distances[start] = 0;
 
-print(distances)
+    priority_queue<pair<int, char>, vector<pair<int, char>>, greater<pair<int, char>>> pq;
+    pq.push({0, start});
+
+    while (!pq.empty()) {
+        int current_distance = pq.top().first;
+        char current_node = pq.top().second;
+        pq.pop();
+
+        if (current_distance > distances[current_node]) {
+            continue;
+        }
+
+        for (const auto& neighbor : graph[current_node]) {
+            char adjacent_node = neighbor.first;
+            int weight = neighbor.second;
+            int distance = current_distance + weight;
+
+            if (distance < distances[adjacent_node]) {
+                distances[adjacent_node] = distance;
+                pq.push({distance, adjacent_node});
+            }
+        }
+    }
+
+    return distances;
+}
+
+int main() {
+    unordered_map<char, unordered_map<char, int>> graph = createGraph();
+    char start_node = 'A';
+    unordered_map<char, int> distances = dijkstra(graph, start_node);
+
+    for (const auto& pair : distances) {
+        cout << pair.first << ": " << pair.second << endl;
+    }
+
+    return 0;
+}
